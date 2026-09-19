e69e1fc4ef0316e09477aba0d47b4a80cacb1ab1
Prithvi Raj <prithvi07raj07@gmail.com>
2026-09-19 16:53:53 +0530

    initiated the project alongside with multer too

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const port = 3000;
+
+
+const uploads = multer({ dest: 'uploads/' });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/upload', uploads.single('pdf'),(req, res)=>{
+    console.log(req.file);
+    res.send('File uploaded successfully');
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

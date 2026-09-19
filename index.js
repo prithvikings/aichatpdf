08699f28e1bbb9215528d956d90f60d1fd72348d
Prithvi Raj <prithvi07raj07@gmail.com>
2026-09-19 17:17:39 +0530

    File upload started working perfectly

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const multer = require('multer');
+
 const app = express();
 const port = 3000;
-
 
 const uploads = multer({ dest: 'uploads/' });
 
@@ -10,10 +10,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/upload', uploads.single('pdf'),(req, res)=>{
-    console.log(req.file);
-    res.send('File uploaded successfully');
-})
+app.post('/upload', uploads.single('pdf'), (req, res) => {
+  console.log(req.file);
+  res.send('File uploaded successfully');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
